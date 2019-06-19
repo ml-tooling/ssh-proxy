d746ea7eea6dd58cd39bf9426aea051679fad94b
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2019-06-19 14:56:47 +0200

    Rename environment variable SSH_PERMIT_SERVICE_PORT to SSH_PERMIT_TARGET_PORT

--- docker-res/start_ssh.py
+++ docker-res/start_ssh.py
@@ -18,12 +18,13 @@ if ENV_SSH_PERMIT_TARGET_HOST == "":
     print("The environment variable {} must be set.".format(ENV_NAME_PERMIT_TARGET_HOST))
     exit(1)
 
-ENV_SSH_PERMIT_SERVICE_PORT = os.getenv("SSH_PERMIT_SERVICE_PORT", "22")
+ENV_NAME_PERMIT_TARGET_PORT = "SSH_PERMIT_TARGET_PORT"
+ENV_SSH_PERMIT_TARGET_PORT = os.getenv(ENV_NAME_PERMIT_TARGET_PORT, "22")
 SSHD_CONFIG = "/etc/ssh/sshd_config"
 
 # replace the PermitOpen directive placeholders in the sshd_config
 call("sed -i 's/{" + ENV_NAME_PERMIT_TARGET_HOST + "}/" + ENV_SSH_PERMIT_TARGET_HOST + "/g' " + SSHD_CONFIG, shell=True)
-call("sed -i 's/{PERMIT_SERVICE_PORT}/" + ENV_SSH_PERMIT_SERVICE_PORT + "/g' " + SSHD_CONFIG, shell=True)
+call("sed -i 's/{" + ENV_NAME_PERMIT_TARGET_PORT + "}/" + ENV_SSH_PERMIT_TARGET_PORT + "/g' " + SSHD_CONFIG, shell=True)
 
 # add environment variables to the script executed by sshd to preserve their values in the ssh session
 call("sed -i \"s@{" + ENV_NAME_PERMIT_TARGET_HOST + "}@" + ENV_SSH_PERMIT_TARGET_HOST + "@g\" /etc/ssh/authorize.sh", shell=True)
